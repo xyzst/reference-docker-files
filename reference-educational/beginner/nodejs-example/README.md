01342c569c292425def163bbeb50ec275bf90fb6
Darren Rambaud <xyzst@users.noreply.github.com>
2019-07-09 21:56:18 -0500

    Slightly complicate application by adding redis dependency. Re-work application to support a visitor counter feature. Automate docker CLI process with docker-compose.yml file. Update readme

--- reference-educational/beginner/nodejs-example/README.md
+++ reference-educational/beginner/nodejs-example/README.md
@@ -2,8 +2,9 @@
 
 1. Checkout repository
 2. Navigate to /reference-educational/beginner/nodejs-example
-3. \$ docker build -t <some_tag_here> .
-4. \$ docker run -p 8080:8080 <tag_from_step_iii>
-5. Navigate to http://localhost:8080
+   ~~3. \$ docker build -t <some_tag_here> .~~
+   ~~4. \$ docker run -p 8080:8080 <tag_from_step_iii>~~
+3. \$ docker-compose up --build
+4. Navigate to http://localhost:8080
 
 NOTE: CTRL+C (SIGINT) to exit or leverage Docker CLI to close the container
--- reference-educational/beginner/nodejs-example/index.js
+++ reference-educational/beginner/nodejs-example/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
-
+const redis = require("redis");
+const client = redis.createClient({
+  host: "redis-server", // This is referencing the alias/hostname defined in the docker-compose.yml file
+  port: 6379 // By default, this value is 6379 anyways but defining here to eliminate any ambiguity since this is a reference project
+});
+client.set("visitors", 0); // Given multiple instances of the express server, this will reset the counter to 0 for every new instance (fix this later)
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Austin, TX!");
+  client.get("visitors", (error, counter) => {
+    res.send(`
+        Hello from Austin, TX! \n
+        You are vistor number: ${counter}
+        `);
+    client.set("visitors", parseInt(counter) + 1);
+  });
 });
 
 app.listen(8080, () => {
