24c37bdf8f9a8e2ab9b9dc7c94b3c5588dadb449
Darren Rambaud <xyzst@users.noreply.github.com>
2019-07-13 15:12:17 -0500

    Setup Fibonnacci Application (#3)
    * Setup daemon process to calculate fibonnaci sequence given an {index} value. Cache calculated value to redis. Setup daemon process to subscribe to redis insertions on the {values} set
    
    * Setup API server with expressjs. Configure client settings for redis, and postgres. Add a few basic route handlers (GET /values/all, GET /, GET /values/current, & POST /values)
    
    * Setup reactjs application w/ new components to create http requests to express server via axios
    
    * Prepend express routes with /api/

---
+++ web-applications/reactjs-expressjs/expressjs/index.js
@@ -0,0 +1,69 @@
+const keys = require("./keys");
+
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres client setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  datababase: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+
+pgClient.on("error", () => console.log("Postgres connection has been lost!"));
+pgClient
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+  .catch(error => console.log(error));
+
+// Redis client setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+// NOTE: need to make duplicate client for publishers/subscribers
+const publisher = redisClient.duplicate();
+
+// Express route handlers
+app.get("/", (request, response) => {
+  response.send("hey!");
+});
+
+app.get("/api/values/all", async (request, response) => {
+  const values = await pgClient.query("SELECT * FROM values");
+  response.send(values.rows);
+});
+
+app.get("/api/values/current", async (request, response) => {
+  redisClient.hgetall("values", (err, values) => {
+    response.send(values);
+  });
+});
+
+app.post("/api/values", async (request, response) => {
+  const index = request.body.index;
+  if (parseInt(index) > 40) {
+    return response.status(422).send("Index too high!");
+  }
+
+  redisClient.hset("values", index, "Nothing yet!");
+  publisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  response.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log("Listening on port 5000 ...");
+});
--- web-applications/reactjs-expressjs/reactjs/src/App.js
+++ web-applications/reactjs-expressjs/reactjs/src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Fibonnacci from "./components/Fibonnacci";
+import OtherPage from "./components/OtherPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from Austin, TX!</p>
-        <p>Signed,</p>
-        <p>Darren Rambaud</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Home</Link>
+        <div>
+          <Route exact path="/" component={Fibonnacci} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
