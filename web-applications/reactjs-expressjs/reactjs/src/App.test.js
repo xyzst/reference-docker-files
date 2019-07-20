370d83f955d93b08effe899014b40ba571fddeed
Darren Rambaud <xyzst@users.noreply.github.com>
2019-07-19 22:11:54 -0500

    Fix typo with "fibonacci"

--- web-applications/reactjs-expressjs/reactjs/src/App.test.js
+++ web-applications/reactjs-expressjs/reactjs/src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 it("renders without crashing", () => {
   // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div); # Will fail because the Fibonnacci component is making a request to a resource. TODO need to mock these requests
+  // ReactDOM.render(<App />, div); # Will fail because the Fibonacci component is making a request to a resource. TODO need to mock these requests
   // ReactDOM.unmountComponentAtNode(div);
 });
